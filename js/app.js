1a1c6dc299ac5e31958722091b1927b2dc9aa53a
Mateo Gomez <mateo.gomez.santa@gmail.com>
2021-01-22 13:47:40 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,35 +7,41 @@ const bannerHtml = `<div class="alert-banner">
 alertBanner.innerHTML = bannerHtml;
 alertBanner.addEventListener('click', (e) => {
     const element = e.target;
-    if(element.classList.contains("alert-banner-close")){
+    if (element.classList.contains("alert-banner-close")) {
         alert.style.display = "none"
     }
 })
 
-let trafficData = {
+const trafficCanvas = document.querySelector("#traffic-chart");
+const dailyCanvas = document.getElementById("daily-chart");
+const mobileCanvas = document.getElementById("mobile-chart");
+
+console.log(trafficCanvas);
+
+const trafficData = {
     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
         "4-10", "11-17", "18-24", "25-31"],
     datasets: [{
-        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
+        data: [500, 1000, 1500, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
             2500],
         backgroundColor: 'rgba(116, 119, 191, .3)',
         borderWidth: 1,
     }]
 };
 
-let trafficOptions = {
+const trafficOptions = {
     aspectRatio: 2.5,
-    animation:{
+    animation: {
         duration: 0
     },
     scales: {
-        yAxes:[{
+        yAxes: [{
             ticks: {
                 beginAtZero: true
             }
         }]
     },
-    legend : {
+    legend: {
         display: false
     }
 };
@@ -46,8 +52,6 @@ let trafficChart = new Chart(trafficCanvas, {
     options: trafficOptions
 });
 
-
-const dailyCanvas = document.getElementById("daily-chart");
 
 const dailyData = {
     labels: ["S", "M", "T", "W", "T", "F", "S"],
@@ -70,3 +74,41 @@ const dailyOptions = {
         display: false
     }
 }
+
+let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
+});
+
+
+
+const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+        label: '# of Users',
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroundColor: [
+            '#7477BF',
+            '#78CF82',
+            '#51B6C8'
+        ]
+    }]
+};
+
+const mobileOptions = {
+    legend: {
+        position: 'right',
+        labels: {
+            boxWidth: 20,
+            fontStyle: 'bold'
+        }
+    }
+}
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
+});
